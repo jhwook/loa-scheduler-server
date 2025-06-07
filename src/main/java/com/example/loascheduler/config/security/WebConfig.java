2e221a745bfa4c90e6aa1b559ac50ff363c2b6e2
jhwook <jhwooook@naver.com>
2025-06-07 18:32:37 +0900

    .

--- src/main/java/com/example/loascheduler/config/security/WebConfig.java
+++ src/main/java/com/example/loascheduler/config/security/WebConfig.java
@@ -1,19 +1,19 @@
-package com.example.loascheduler.config.security;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        // 모든 요청에 대해 CORS 허용
-        registry.addMapping("/**")
-                .allowedOrigins("https://jhwook.github.io", "https://loa-scheduler.com", "https://www.loa-scheduler.com", "http://localhost:3000") // 클라이언트의 URL을 지정
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
-                .allowedHeaders("*") // 허용할 헤더
-                .allowCredentials(true); // 쿠키 포함 허용
-    }
-}
+//package com.example.loascheduler.config.security;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        // 모든 요청에 대해 CORS 허용
+//        registry.addMapping("/**")
+//                .allowedOrigins("https://jhwook.github.io", "https://loa-scheduler.com", "https://www.loa-scheduler.com", "http://localhost:3000") // 클라이언트의 URL을 지정
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+//                .allowedHeaders("*") // 허용할 헤더
+//                .allowCredentials(true); // 쿠키 포함 허용
+//    }
+//}
